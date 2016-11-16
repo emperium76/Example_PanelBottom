eb00031c7e08bf1165a295cedc79c1b25e8234b6
emperium76 <Tommy@Joshuas-Mac.local>
2016-11-16 11:41:58 +0700

    convert into swift 3

--- testPanelBottom/ViewController.swift
+++ testPanelBottom/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var panelViewTopConstraint: NSLayoutConstraint!
     
     lazy var panGestureRecognizer: UIPanGestureRecognizer = {
-        let gr = UIPanGestureRecognizer(target: self, action: "panGesture:")
+        let gr = UIPanGestureRecognizer(target: self, action: #selector(ViewController.panGesture(_:)))
         self.panelView.addGestureRecognizer(gr)
         return gr
     }()
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
         super.viewWillLayoutSubviews()
         collectionView.collectionViewLayout.invalidateLayout()                  // to refresh cell size on device rotation
         let currentPage = collectionView.contentOffset.x / collectionView.bounds.width
-        NSOperationQueue.mainQueue().addOperationWithBlock { 
+        OperationQueue.main.addOperation { 
             self.collectionView.setContentOffset(CGPoint(x: currentPage * self.collectionView.bounds.width, y: 0), animated: false)
         }
     }
@@ -41,7 +41,7 @@ class ViewController: UIViewController {
         originalPanelAlpha = panelView.alpha
     }
 
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.nameLabel.text = panelModel.getName()
         self.summaryLabel.text = panelModel.getTextSummary()
@@ -50,30 +50,30 @@ class ViewController: UIViewController {
 
     var originalPanelAlpha: CGFloat = 0
     var originalPanelPosition: CGFloat = 0
-    var lastPoint: CGPoint = CGPointZero
+    var lastPoint: CGPoint = CGPoint.zero
     
-    func setViewAlphas(centerRatio centerRatio: CGFloat) {
+    func setViewAlphas(centerRatio: CGFloat) {
         panelView.alpha = originalPanelAlpha + (centerRatio * (1.0 - originalPanelAlpha))
         let howFarFromCenterRatio = 0.5 - centerRatio
         summaryLabel.alpha = howFarFromCenterRatio * 2
         longTextLabel.alpha = -howFarFromCenterRatio * 2
     }
     
-    func panGesture(gestureRecognizer: UIPanGestureRecognizer) {
-        let point = gestureRecognizer.locationInView(self.view)
+    func panGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
+        let point = gestureRecognizer.location(in: self.view)
         let screenHeight = collectionView.bounds.height
         let centerRatio = (-panelViewTopConstraint.constant + originalPanelPosition) / (screenHeight + originalPanelPosition)
         switch gestureRecognizer.state {
-        case .Changed:
+        case .changed:
             let yDelta = point.y - lastPoint.y
             var newConstant = panelViewTopConstraint.constant + yDelta
             newConstant = newConstant > originalPanelPosition ? originalPanelPosition : newConstant
             newConstant = newConstant < -screenHeight ? -screenHeight : newConstant
             panelViewTopConstraint.constant = newConstant
             setViewAlphas(centerRatio: centerRatio)
-        case .Ended:
+        case .ended:
             self.panelViewTopConstraint.constant = centerRatio < 0.5 ? self.originalPanelPosition : -screenHeight
-            UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8, options: [.CurveEaseInOut], animations: {
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8, options: UIViewAnimationOptions(), animations: {
                 self.view.layoutIfNeeded()
                 self.setViewAlphas(centerRatio: centerRatio < 0.5 ? 0.0 : 1.0)
                 }, completion: nil)
@@ -87,23 +87,23 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return panelModel.getImages().count
     }
     
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("image", forIndexPath: indexPath) as! ImageCollectionViewCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "image", for: indexPath) as! ImageCollectionViewCell
         if let img = panelModel.getImages()[indexPath.item] {
             cell.imageView.image = img
         }
         return cell
     }
     
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return collectionView.bounds.size
     }
     
